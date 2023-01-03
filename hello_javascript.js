23bbedbf851e3a0fb7ffbae28ab518f36d9c2531
lynnily <lynnilyh@gmail.com>
2023-01-03 23:47:01 +0900

    Ch.3

--- hello_javascript.js
+++ hello_javascript.js
@@ -1,5 +1,68 @@
-var i = 0;
-for (i=0; i<=10; i++) {
-    document.write("Hello javascript!");
-    document.write("</br>")
+/*var intNum = 10;
+var floatNum = 0.1;
+
+var singleQuoteStr = 'single quote string';
+var doubleQuoteStr = "double quote string";
+var singleChar = 'a';
+
+var boolVar = true;
+
+var emptyVar;
+
+var nullVar = null;
+
+console.log(
+    typeof intNum,
+    typeof floatNum,
+    typeof singleQuoteStr,
+    typeof doubleQuoteStr,
+    typeof singleChar,
+    typeof boolVar,
+    typeof emptyVar,
+    typeof nullVar
+);*/
+
+/*var str = 'test';
+console.log(str);
+str = 'TEST'
+console.log(str[0]) */
+
+/*var foo = new Object();
+foo.name = 'foo';
+foo.age = 30;
+foo.gender = 'male'
+console.log(typeof foo);
+console.log(foo);*/
+
+/*var foo = {
+    name : 'foo',
+    age : 40,
+    gender : 'male',
+    'full-name' : 'foo bar'
 }
+var name = 'full-name';
+console.log(foo['name']);
+console.log(foo[name]);
+
+var prop;
+for (prop in foo) {
+    console.log(prop, foo[prop]);
+}*/
+
+/*var a = 100;
+var objA = {value: 100};
+function changeArg(num, obj) {
+    num = 200;
+    obj.value = 200;
+    console.log('num:', num, 'obj:', obj);
+}
+
+changeArg(a, objA);
+console.log('a:', a, 'objA:', objA);*/
+
+var emptyArr = [];
+console.log(emptyArr[0], emptyArr[1]);
+
+emptyArr[0] = 100;
+emptyArr[1] = 'eight';
+
